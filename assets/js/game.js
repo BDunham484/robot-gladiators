706d927a5c6121eb4ad690cdc296fc2fc95c989d
ThereWolf <bdunham484@gmail.com>
2022-02-15 11:10:35 -0600

    update js function

--- assets/js/game.js
+++ assets/js/game.js
@@ -76,8 +76,23 @@ var fight = function(enemyName) {
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    // debugger;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        //let player know what round they are in, remember the arrays start at 0
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1));
+
+        //pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        //debugger;
+
+        // pass the pickedEnemyname variable's value into the fight function, where it will assume the alue of the enemyName parameter
+        fight(pickedEnemyName);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over, Man!");
+        break;
+    }
 }
